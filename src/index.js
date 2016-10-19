c296f45b40a8814a8c9d67eceb160685e24e55a0
PALO IT-Andrian <paloit-andrian@PALOs-MBP.singnet.com.sg>
2016-10-19 10:31:45 +0800

    added a css styling for accordion

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import HelloWorld from './components/sample-component/sample-component'
-import Table from './components/table/table'
+import Table from './components/table/table-component'
 
 class AppThisLOL extends React.Component{
     constructor(props){
