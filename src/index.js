a02a17a1e153f173d1e00eefc008bc67298cca88
PALO IT-Andrian <paloit-andrian@PALOs-MBP.singnet.com.sg>
2016-10-19 09:24:32 +0800

    new table

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import HelloWorld from './components/sample-component/sample-component'
+import Table from './components/table/table'
 
 class AppThisLOL extends React.Component{
     constructor(props){
@@ -10,9 +11,8 @@ class AppThisLOL extends React.Component{
     render(){
         return (
             <div>
-
-
                 <HelloWorld />
+                <Table />
             </div>
         )
     }
