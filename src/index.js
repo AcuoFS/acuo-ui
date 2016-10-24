a03896e2a67ff830f108cf64ca2b9fbd586ea454
Rui <rchai@palo-it.com>
2016-10-24 16:49:09 +0800

    initial state set with json implemented redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import {fromJS} from 'immutable'
+
+import reducer from './reducer'
+import {initState} from './action_creators'
 
 import styles from './global.css'
 
@@ -7,11 +12,19 @@ import HelloWorld from './components/sample-component/sample-component'
 import Table from './components/table/table-component'
 import Nav from './components/navbar/navbar'
 
+const store = createStore(reducer)
 
 class App extends React.Component{
     constructor(props){
         super(props)
+
+        fetch('https://acuo.herokuapp.com/json').then((response) => {
+            return response.json()
+        }).then((obj) => {
+            store.dispatch(initState(fromJS(obj)))
+        })
     }
+
     render(){
         return (
             <div className={styles.globalStyles}>
