8bc3a36497f2077b5f60ac2c9af3f7479d13e9a0
Rui <rchai@palo-it.com>
2016-10-25 17:44:58 +0800

    sample son for hicham

--- src/action_creators.js
+++ src/action_creators.js
@@ -1,7 +1,7 @@
 export function initState(state) {
 
-    return {
-        type: 'INIT_STATE',
-        state: state
-    }
+  return {
+    type: 'INIT_STATE',
+    state: state
+  }
 }
--- src/index.js
+++ src/index.js
@@ -15,25 +15,25 @@ import Nav from './components/navbar/navbar'
 const store = createStore(reducer)
 
 class App extends React.Component{
-    constructor(props){
-        super(props)
+  constructor(props){
+    super(props)
 
-        fetch('https://acuo.herokuapp.com/json').then((response) => {
-            return response.json()
-        }).then((obj) => {
-            store.dispatch(initState(fromJS(obj)))
-        })
-    }
+    fetch('https://acuo.herokuapp.com/json').then((response) => {
+      return response.json()
+    }).then((obj) => {
+      store.dispatch(initState(fromJS(obj)))
+    })
+  }
 
-    render(){
-        return (
-            <div className={styles.globalStyles}>
-                <Nav />
-                <HelloWorld />
-                <Table title={"Margin"}/>
-            </div>
-        )
-    }
+  render(){
+    return (
+      <div className={styles.globalStyles}>
+          <Nav />
+          <HelloWorld />
+          <Table title={"Margin"}/>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
