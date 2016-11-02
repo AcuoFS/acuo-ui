87245c135f3d328cacfa8a9b14dc51762186e7f9
Vikas Saryal <vsaryal@palo-it.com>
2016-11-02 11:18:15 +0800

    Added code for router

---
+++ src/components/reconcile/reconcile.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vikassaryal on 2/11/16.
+ */
+import React from 'react'
+import { render } from 'react-dom'
+
+
+export class Reconcile extends React.Component{
+
+
+  render(){
+    return(<div>
+    <h1>Welcome to Reconcile Page</h1>
+    </div>)
+  }
+}
