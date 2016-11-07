70596913896dcd7f46623cc7e9e2ddd6d3b51dde
Pan Yong <apan@palo-it.com>
2016-11-07 12:48:29 +0800

    recon page structure and css refined for dashboard page

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -4,6 +4,7 @@ import {FilterContainer} from '../shared/filters/filter'
 import {GraphContainer} from './graph/graph'
 import {TableContainer} from './table/table-component'
 import UploadPortfolio from '../../components/dashboard/uploadPortfolio/upload_portfolio'
+
 class Dashboard extends React.Component{
     render(){
         return (<div>
