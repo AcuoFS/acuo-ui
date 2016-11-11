5fe7e4475363f45706b80a3a0405b4ba063f12d3
Pan Yong <apan@palo-it.com>
2016-11-11 11:14:43 +0800

    (OW-58) switch +/- to show/hide 2nd level packages

--- src/components/reconcile/actions/actionlineitem-component.js
+++ src/components/reconcile/actions/actionlineitem-component.js
@@ -10,35 +10,73 @@ export default class ActionLineItem extends React.Component {
         super(props)
         this.state = {
             open: false,
-            checkbox: "./images/reconcile/checkbox.png"
+            expand: "./images/reconcile/plus.png",
+            checkbox: "./images/reconcile/checkbox.png",
+            cbTicked: false,
+            cbLvl1: styles.show,
+            pkgLvl2: styles.hide
         }
         this.handleClick = this.handleClick.bind(this)
-
+        this.handlePlusMinus = this.handlePlusMinus.bind(this)
     }
     handleClick(){
-        if(this.state.open){
+        if(this.state.cbTicked){
             this.setState({
-                open: false,
+                cbTicked: false,
                 checkbox: "./images/reconcile/checkbox.png"
             })
         }else {
             this.setState({
-                open: true,
+                cbTicked: true,
                 checkbox: "./images/reconcile/checkboxwithtick.png"
             });
         }
     }
+    handlePlusMinus(){
+        if(this.state.open){
+            this.setState({
+                open: false,
+                expand: "./images/reconcile/plus.png",
+                cbLvl1: styles.show,
+                pkgLvl2: styles.hide
+            })
+        }else {
+            this.setState({
+                open: true,
+                expand: "./images/reconcile/minus.png",
+                cbLvl1: styles.hide,
+                pkgLvl2: styles.show
+            })
+        }
+    }
     render(){
         return(
-        <div className={styles.packageRow}> {/* one row div*/}
-            <div className={styles.packageLeft}>
-                <div className={styles.packageCheckBox} onClick={this.handleClick}>
-                    <img src={this.state.checkbox} alt=""/>
+        <div>
+            <div className={styles.packageRow}> {/* one row div*/}
+                <div className={styles.packageLeft}>
+                    <div className={styles.packageCheckBox+' '+this.state.cbLvl1} onClick={this.handleClick}>
+                        <img src={this.state.checkbox} alt=""/>
+                    </div>
+                    <div className={styles.packageText}>Net Total IM</div>
+                    <ActionLineItemExpand doClick={this.handlePlusMinus} image={this.state.expand}/>
                 </div>
-                <div className={styles.packageText}>Net Total IM</div>
-                <ActionLineItemExpand/>
+                <div className={styles.packageRight}> 15,586,933</div>
             </div>
-            <div className={styles.packageRight}> 15,586,933</div>
+
+            <div className={styles.packageLvl2+' '+this.state.pkgLvl2}>
+
+                {/* have second level table rendering structure here */}
+                <div className={styles.packageRow}>
+                    <div className={styles.packageLeft}>
+                        <div className={styles.packageCheckBox} onClick={this.handleClick}>
+                            <img src={this.state.checkbox} alt=""/>
+                        </div>
+                        <div className={styles.packageText}>asda adqwewqer rewr</div>
+                    </div>
+                    <div className={styles.packageRight}> XX,XXX,XXX</div>
+                </div>
+
+            </div>
         </div>
         )
     }
