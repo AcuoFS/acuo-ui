acb6e0b6ae701c72f1d873066e544b9e179b53bf
Rui <rchai@palo-it.com>
2016-11-24 16:30:28 +0800

    OW-105 sliders now working

--- src/components/pledge/sub-components/OptItem.js
+++ src/components/pledge/sub-components/OptItem.js
@@ -2,30 +2,69 @@
  * Created by panyong on 22/11/16.
  */
 import React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import styles from '../Pledge.css'
 
-class OptItem extends React.Component{
-    constructor(props) {
-        super(props)
-    }
+class OptItem extends React.Component {
+  constructor(props) {
+    super(props)
 
-    render() {
-        return (
-            <div className={styles.optItem}>
-                <div className={styles.optItemName}>{this.props.sldName}</div>
-                <div className={styles.optItemSlider}>
-                    <div className={styles.sliderUnit}>
+    this.dragg = this.dragg.bind(this)
+  }
 
-                    </div>
-                    <div className={styles.sliderLine}>
-                        <div className={styles.sliderLineFill}></div>
-                    </div>
-                    <div className={styles.sliderHandle}></div>
-                </div>
+  componentWillMount() {
+    this.setState({
+      drag: (5).toFixed(1)
+    })
+  }
+
+  dragg(e) {
+    this.setState({drag: (e.currentTarget.value / 10).toFixed(1)})
+  }
+
+  render() {
+    return (
+      <div className={styles.optItem}>
+
+        <div className={styles.flexContainer}>
+          <div className={styles.optItemName}>{this.props.sldName}</div>
+          <div className={styles.optItemSlider}>
+            <input type="range" step="1" max="100" min="0" onChange={this.dragg}/>
+            <div className={styles.markings}>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.marking}>|</div>
+              <div className={styles.miniMarking}>|</div>
+              <div className={styles.markingLast}>|</div>
             </div>
-        )
-    }
+          </div>
+        </div>
+
+        <div className={styles.dragValue}>
+          <div className={styles.valueBox}>
+            {this.state.drag}
+          </div>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default OptItem
