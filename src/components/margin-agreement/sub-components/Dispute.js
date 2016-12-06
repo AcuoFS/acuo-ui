1d6a1810f820891d0d4c3988b1b3df406f24708d
Achal Kagwad <akagwad@palo-it.com>
2016-12-06 17:18:18 +0800

    OW-31 Bug Fix to enable the Dispute Button

--- src/components/margin-agreement/sub-components/Dispute.js
+++ src/components/margin-agreement/sub-components/Dispute.js
@@ -105,7 +105,7 @@ export default class Dispute extends React.Component {
 
 
 
-        <div className={styles.buttonContainer}>
+        <div className={styles.buttonContainerDisabled}>
           <button type="submit">Dispute</button>
         </div>
 
