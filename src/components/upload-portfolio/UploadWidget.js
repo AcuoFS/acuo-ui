b69e594aab51c7766d28f6ace7a31e1eedd985dc
Jun Jie Ng <jjng@palo-it.com>
2016-12-09 10:22:01 +0800

    Change for OW-97, 114, 115 and 116

--- src/components/upload-portfolio/UploadWidget.js
+++ src/components/upload-portfolio/UploadWidget.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import DropzoneComponent from 'react-dropzone-component';
+import ReactDOMServer from 'react-dom/server'
+import DropzoneComponent from 'react-dropzone-component'
+import styles from './UploadWidget.css'
+
 
 export default class UploadWidget extends React.Component {
   constructor(props) {
@@ -11,23 +14,44 @@ export default class UploadWidget extends React.Component {
       autoProcessQueue: false,
       maxFiles: 5,
       parallelUploads: 5
+      , dictDefaultMessage: "Drag and drop portfolio files, or click anywhere here"
+      , previewTemplate: ReactDOMServer.renderToStaticMarkup(
+          <div className={"dz-preview dz-file-preview " + styles.alignFileIconLeft}>
+            <div className="dz-details">
+              <div className="dz-filename"><span data-dz-name="true"></span></div>
+              <img data-dz-thumbnail="true" src="./images/upload-portfolio/file_icon.png"/>
+              <img src="./images/upload-portfolio/cross_cancel.png" alt="Click me to remove the file." data-dz-remove/>
+            </div>
+            <div className="dz-progress"><span className="dz-upload" data-dz-uploadprogress="true"></span></div>
+            <div className="dz-success-mark"><span>✔</span></div>
+            <div className="dz-error-mark"><span>✘</span></div>
+            <div className="dz-error-message"><span data-dz-errormessage="true"></span></div>
+          </div>
+      )
+      , clickable: false
     }
 
     this.componentConfig = {
       iconFiletypes: ['.xlsx'],
-      showFiletypeIcon: true,
+      showFiletypeIcon: false,
       postUrl: 'http://127.0.0.1:3000/uploadHandler'
     }
 
     this.dropzone = null
-    this.onButtonClicked = this.onButtonClicked.bind(this)
+    this.onGenerate = this.onGenerate.bind(this)
   }
 
   handleFileAdded(file) {
     console.log(file)
   }
 
-  onButtonClicked() {
+  success(file) {
+    console.log('uploaded', file)
+    setTimeout(function (){this.dropzone.removeFile(file)}.bind(this), 2000)
+    setTimeout((() => {this.dropzone.removeFile(file)}).bind(this), 1500)
+  }
+
+  onGenerate() {
     console.log(this.componentConfig.postUrl)
     this.dropzone.processQueue()
   }
@@ -38,7 +62,8 @@ export default class UploadWidget extends React.Component {
 
     const eventHandlers = {
       init: dz => this.dropzone = dz,
-      addedfile: this.handleFileAdded.bind(this)
+      addedfile: this.handleFileAdded.bind(this),
+      success: this.success.bind(this)
     }
 
     return (
@@ -46,9 +71,12 @@ export default class UploadWidget extends React.Component {
       <input type="file" id="myFile"/>
       <input type="submit" value="Submit"/>
     </form>*/
-      <div>
-        <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig}/>
-        <button type="button" onClick={this.onButtonClicked}>Click Me!</button>
+      <div className={styles.componentStyle}>
+        <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig}>
+          <div className="dz-message">Drag and drop portfolio files, or <a href="#">browse</a> .</div>
+        </DropzoneComponent>
+        <button type="button" onClick={this.onGenerate}>Generate Margin Call Data</button>
+        <div className={styles.cloudImage}></div>
       </div>
     )
   }
