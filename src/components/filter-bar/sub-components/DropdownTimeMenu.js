f87b5412ba3eb8e6376ea57d9be8e1df5944cd6d
Rui <rchai@palo-it.com>
2017-01-19 10:37:50 +0800

    OW-360 OW-361 dynamic arrows

--- src/components/filter-bar/sub-components/DropdownTimeMenu.js
+++ src/components/filter-bar/sub-components/DropdownTimeMenu.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import styles from '../FilterBar.css'
 
-export default class DropdownTimeMenu extends React.Component{
+export default class DropdownTimeMenu extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       timeWindowTitle: 'today',
-      timeArrowLeft: styles.show,
-      timeArrowRight: styles.show
     }
     this.renderPrevDay = this.renderPrevDay.bind(this)
     this.renderNextDay = this.renderNextDay.bind(this)
@@ -22,8 +20,6 @@ export default class DropdownTimeMenu extends React.Component{
         return (
           this.setState({
             timeWindowTitle: 'yesterday',
-            timeArrowLeft: styles.hide,
-            timeArrowRight: styles.show
           })
         )
 
@@ -31,8 +27,6 @@ export default class DropdownTimeMenu extends React.Component{
         return (
           this.setState({
             timeWindowTitle: 'tomorrow',
-            timeArrowLeft: styles.show,
-            timeArrowRight: styles.hide
           })
         )
 
@@ -40,8 +34,6 @@ export default class DropdownTimeMenu extends React.Component{
         return (
           this.setState({
             timeWindowTitle: 'today',
-            timeArrowLeft: styles.show,
-            timeArrowRight: styles.show
           })
         )
     }
@@ -69,63 +61,62 @@ export default class DropdownTimeMenu extends React.Component{
     return e.stopPropagation()
   }
 
-  onMenuOptionSelect(handleOnOptionChange, minTime, maxTime, string){
-
-    // let currTime = new Date('2017-01-13T08:00:00.000Z')
-    // if (e.currentTarget.dataset.min == 'All') {
-    //   handleOnOptionChange(e,
-    //     this.state.timeWindowTitle + ": " + e.currentTarget.dataset.min,
-    //     this.state.timeWindowTitle + ":" + e.currentTarget.dataset.min,
-    //     null)
-    // }
-    // else {
-    //   if (this.state.timeWindowTitle == 'yesterday') currTime.setDate(currTime.getDate() - 1)
-    //   else if (this.state.timeWindowTitle == 'tomorrow') currTime.setDate(currTime.getDate() + 1)
-    //   let minTimeRange = new Date(currTime.setHours(e.currentTarget.dataset.min, 0, 0))
-    //   let maxTimeRange = new Date(currTime.setHours(e.currentTarget.dataset.max, 0, 0))
-
-      handleOnOptionChange(this.state.timeWindowTitle + ": " + string, minTime, maxTime)
-    //}
+  onMenuOptionSelect(handleOnOptionChange, minTime, maxTime, string) {
+    handleOnOptionChange(this.state.timeWindowTitle + ": " + string, minTime, maxTime)
   }
 
-  checkShow(day, stateDay){
-    if(day == stateDay)
+  checkShow(day, stateDay) {
+    if (day == stateDay)
       return styles.timeShow
     else
       return ''
   }
 
-  render(){
+  checkTimesList(obj) {
+    if (obj)
+      if (obj.times.length)
+        return styles.show
+      else
+        return styles.hide
+    else return styles.hide
+  }
+
+  render() {
     const {handleOnOptionChange, options} = this.props
     // merge option 'ALL', with actual options
     const optionList = options
 
-    return(
+    return (
       <div>
-      {optionList.map((option, index) => {
+        {optionList.map((option, index) => {
 
-        if(option.times.length > 0)
-          return (<ul key={index} className={styles.filtersList + ' ' + styles.timeSlot + ' ' + this.checkShow(option.day, this.state.timeWindowTitle)}>
+          if (option.times.length > 0)
+            return (
+              <ul key={index}
+                  className={styles.filtersList + ' ' + styles.timeSlot + ' ' + this.checkShow(option.day, this.state.timeWindowTitle)}>
 
-            <li className={styles.timeTitle} onClick={this.preventClose}>
-              <div className={styles.timeArrowLeft + ' ' + this.state.timeArrowLeft} onClick={this.renderPrevDay}></div>
-              <span>{this.state.timeWindowTitle}</span>
-              <div className={styles.timeArrowRight + ' ' + this.state.timeArrowRight} onClick={this.renderNextDay}></div>
-            </li>
+                <li className={styles.timeTitle} onClick={this.preventClose}>
+                  <div className={styles.timeArrowLeft + ' ' + this.checkTimesList(optionList[index - 1])}
+                       onClick={this.renderPrevDay}></div>
+                  <span>{this.state.timeWindowTitle}</span>
+                  <div className={styles.timeArrowRight + ' ' + this.checkTimesList(optionList[index + 1])}
+                       onClick={this.renderNextDay}></div>
+                </li>
 
-            <li onClick={ e => this.onMenuOptionSelect(handleOnOptionChange, option.minTime, option.maxTime, 'All')}>
-              All
-            </li>
+                <li
+                  onClick={ e => this.onMenuOptionSelect(handleOnOptionChange, option.minTime, option.maxTime, 'All')}>
+                  All
+                </li>
 
-            {option.times.map((time, index) => (
-              <li key={index}
-                  onClick={ e => this.onMenuOptionSelect(handleOnOptionChange, time.min, time.max, String((new Date(time.min)).getHours() + ':00 Hrs').toUpperCase())}>
-                {String((new Date(time.min)).getHours() + ':00 Hrs').toUpperCase()}
-              </li>
-            ))}
-          </ul>)
+                {option.times.map((time, index) => (
+                  <li key={index}
+                      onClick={ e => this.onMenuOptionSelect(handleOnOptionChange, time.min, time.max, String((new Date(time.min)).getHours() + ':00 Hrs').toUpperCase())}>
+                    {String((new Date(time.min)).getHours() + ':00 Hrs').toUpperCase()}
+                  </li>
+                ))}
 
-      })}
+              </ul>)
+        })}
       </div>
     )
   }
