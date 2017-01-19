196d5f3db2e66257ccb4484a32d25a6ffad1a65d
Jun Jie Ng <jjng@palo-it.com>
2017-01-19 16:58:43 +0800

    OW-162 Confirm button function on deselection popup

--- src/components/pledge-selection/DeselectionPopup.js
+++ src/components/pledge-selection/DeselectionPopup.js
@@ -3,35 +3,96 @@ import styles from './DeselectionPopup.css'
 
 
 export default class DeselectionPopup extends React.Component {
+  constructor() {
+    super()
+
+    this.radioCurDom = null
+    this.radioAllDom = null
+  }
+
+  validateForm(radioDom, propHandlerSetFormValidity) {
+    if (radioDom && radioDom.checked) {
+      propHandlerSetFormValidity(true)
+    }
+  }
+
+  closePopup(comp, propHandlerClearPopup, radioCurDom, radioAllDom) {
+    // Execute parent's handler
+    propHandlerClearPopup()
+
+    // Clear values
+    comp.setState({isValidForm: false})
+    if(radioCurDom){
+      radioCurDom.checked = false
+    }
+    if(radioAllDom){
+      radioAllDom.checked = false
+    }
+  }
+
   render() {
     const {
       propOpenedDeselectionPopup,
-      propHandlerClearPopup
+      propHandlerClearPopup,
+      propIsValidFlag,
+      propHandlerSetFormValidity
     } = this.props
+
+    // reset values of radio buttons
+    if(propOpenedDeselectionPopup == ''){
+      if(this.radioCurDom){
+        this.radioCurDom.checked = false
+      }
+      if(this.radioAllDom){
+        this.radioAllDom.checked = false
+      }
+    }
 
     return (
       <div className={styles.popupPanel + ' ' +
       ((propOpenedDeselectionPopup == '') ? '' : styles.popupShow)}>
-        <div className={styles.closeButton} onClick={propHandlerClearPopup}>
+        <div className={styles.closeButton} onClick={
+          () => this.closePopup(this, propHandlerClearPopup, this.radioCurDom, this.radioAllDom)
+        }>
           x
         </div>
         <div className={styles.popupBody}>
           Exclude this asset from:
           <div className={styles.popupRow}>
             <div className={styles.rowRadio}>
-              <input type="radio" name="excludeOption" value="current"/>
+              <input type="radio" name="excludeOption" value="current"
+                     ref={dom => this.radioCurDom = dom}
+                     onChange={() => {
+                       this.validateForm(this.radioCurDom, propHandlerSetFormValidity)
+                     }}/>
             </div>
             <div className={styles.rowText}>This margin call only</div>
           </div>
           <div className={styles.popupRow}>
             <div className={styles.rowRadio}>
-              <input type="radio" name="excludeOption" value="all"/>
+              <input type="radio" name="excludeOption" value="all"
+                     ref={dom => this.radioAllDom = dom}
+                     onChange={() => {
+                       this.validateForm(this.radioAllDom, propHandlerSetFormValidity)
+                     }}/>
             </div>
             <div className={styles.rowText}>All margin calls</div>
           </div>
 
           <div>
-            <button type="submit" className={styles.buttonStyle}>
+            <button type="submit" className={styles.buttonStyle + ' ' +
+            (propIsValidFlag ? styles.buttonEnabled : '')}
+                    disabled={!propIsValidFlag}
+                    onClick={() => {
+                      let checkMsg = ''
+                      if (this.radioAllDom.checked) {
+                        checkMsg = 'All calls is selected'
+                      }
+                      if (this.radioCurDom.checked) {
+                        checkMsg = 'Current call is selected'
+                      }
+                      alert('Confirm button clicked. id: ' + propOpenedDeselectionPopup + ', ' + checkMsg)
+                    }}>
               Confirm
             </button>
           </div>
--- src/components/pledge-selection/Selection.js
+++ src/components/pledge-selection/Selection.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { numberWithCommas } from '../../utils/numbersWithCommas'
+import {numberWithCommas} from '../../utils/numbersWithCommas'
 import DeselectionPopup from './DeselectionPopup'
-import { List, toJS } from 'immutable'
+import {List, toJS} from 'immutable'
 import styles from './Selection.css'
 
 export default class Selection extends React.Component {
@@ -9,18 +9,20 @@ export default class Selection extends React.Component {
     super(props)
 
     this.state = {
-      openedDeselectionPopup: ""
+      openedDeselectionPopup: "",
+      isValidPopupForm: false
     }
 
     this.togglePendingAllocation = this.togglePendingAllocation.bind(this)
     this.setDeselectionPopup = this.setDeselectionPopup.bind(this)
     this.clearDeselectionPopup = this.clearDeselectionPopup.bind(this)
     this.handlerChangeSideWaysClick = this.handlerChangeSideWaysClick.bind(this)
+    this.setPopupFormValidity = this.setPopupFormValidity.bind(this)
   }
 
   renderGroup(x, GUID) {
     return (
-      <div className={styles.group} key={x.get('groupName')+GUID}>
+      <div className={styles.group} key={x.get('groupName') + GUID}>
         {this.checkIfExist(x.get('data')).map((y, index) => {
           return (
             <div className={styles.firstLevel} key={index}>
@@ -38,7 +40,7 @@ export default class Selection extends React.Component {
     )
   }
 
-  renderMargin(asset, mgnType){
+  renderMargin(asset, mgnType) {
     const popupID = asset.get('GUID') + mgnType + asset.get('assetName')
 
     return (
@@ -52,7 +54,9 @@ export default class Selection extends React.Component {
         <td>{asset.get('venue')}</td>
         <td>
           <div className={styles.earmarkAssetButton}
-               onClick={() => {this.setDeselectionPopup(popupID)}}>
+               onClick={() => {
+                 this.setDeselectionPopup(popupID)
+               }}>
             <img src="./images/pledge/cancel.png"></img>
             <div className={styles.tooltip}>
               Move to Earmarked
@@ -64,22 +68,25 @@ export default class Selection extends React.Component {
     )
   }
 
-  togglePendingAllocation(e){
+  togglePendingAllocation(e) {
     this.props.onTogglePendingAllocation(e.currentTarget.dataset.ref)
   }
 
   //generic checker
-  checkIfExist(something){
+  checkIfExist(something) {
     return something || List()
   }
 
-  calSubTotal(a, s){
-    if(a.getIn(['allocated', s]))
-      return (a.getIn(['allocated', s]).reduce((SumX , x)=>{return SumX + parseFloat(x.get('price'))},0))
+  calSubTotal(a, s) {
+    if (a.getIn(['allocated', s]))
+      return (a.getIn(['allocated', s]).reduce((SumX, x) => {
+        return SumX + parseFloat(x.get('price'))
+      }, 0))
     else
       return 0
   }
-  calTotal(a, i, j){
+
+  calTotal(a, i, j) {
     return this.calSubTotal(a, i) + this.calSubTotal(a, j)
   }
 
@@ -91,7 +98,8 @@ export default class Selection extends React.Component {
 
   clearDeselectionPopup() {
     this.setState({
-      openedDeselectionPopup: ""
+      openedDeselectionPopup: "",
+      isValidPopupForm: false
     })
   }
 
@@ -104,8 +112,12 @@ export default class Selection extends React.Component {
     }
   }
 
+  setPopupFormValidity(isValid) {
+    this.setState({isValidPopupForm: isValid})
+  }
+
   render() {
-    const { marginCall, pendingAllocationStore } = this.props
+    const {marginCall, pendingAllocationStore} = this.props
 
     let evlEmptyForIntMargin = this.checkIfExist(marginCall.getIn(['allocated', 'initialMargin'])).isEmpty()
     let evlEmptyForVariMargin = this.checkIfExist(marginCall.getIn(['allocated', 'variationMargin'])).isEmpty()
@@ -114,12 +126,16 @@ export default class Selection extends React.Component {
     return (
       <div className={styles.panel} key={marginCall.get('GUID')}>
         <DeselectionPopup propOpenedDeselectionPopup={this.state.openedDeselectionPopup}
-                          propHandlerClearPopup={this.clearDeselectionPopup}/>
+                          propHandlerClearPopup={this.clearDeselectionPopup}
+                          propIsValidFlag={this.state.isValidPopupForm}
+                          propHandlerSetFormValidity={this.setPopupFormValidity}/>
 
         <div className={styles.columnContainer}>
           <div className={styles.leftColumn + ' ' + (!this.props.toggleL ? styles.bigger : '')}>
             <div className={styles.titleHolder}>
-              <img src={(this.checkIfExist(pendingAllocationStore).includes(marginCall.get('GUID')) ? "./images/pledge/checkboxwithtick.png" : "./images/pledge/checkbox.png")} className={styles.selTick} onClick={this.togglePendingAllocation} data-ref={marginCall.get('GUID')}/>
+              <img
+                src={(this.checkIfExist(pendingAllocationStore).includes(marginCall.get('GUID')) ? "./images/pledge/checkboxwithtick.png" : "./images/pledge/checkbox.png")}
+                className={styles.selTick} onClick={this.togglePendingAllocation} data-ref={marginCall.get('GUID')}/>
               <span className={styles.panelTitle}>{marginCall.get('GUID')}</span>
               <div className={styles.subtitle}>
                 {marginCall.get('legalEntity')} - {marginCall.get('GUID')}
@@ -143,10 +159,10 @@ export default class Selection extends React.Component {
                 <div className={styles.amount}>
                   {numberWithCommas(this.checkIfExist(marginCall.get('clientAssets')).reduce((sum, x) => {
                     return sum + x.get('data').reduce((sum, y) => {
-                      return sum + y.get('secondLevel').reduce((sum, z) => {
-                        return sum + parseFloat(z.get('amount'))
-                        }, 0)
-                    }, 0)
+                        return sum + y.get('secondLevel').reduce((sum, z) => {
+                            return sum + parseFloat(z.get('amount'))
+                          }, 0)
+                      }, 0)
                   }, 0))}
                 </div>
               </div>
@@ -169,8 +185,8 @@ export default class Selection extends React.Component {
             <div className={styles.ttlMarginWrap + ' ' + (this.props.toggleR ? styles.showR : styles.hideR)}>
               <div className={styles.ttlMargin}>
                 <div>Total Allocated</div>
-                <div className={styles.bigFig + ' ' +styles.bold}>
-                  {Math.round((marginCall.getIn(['allocated', 'marginTotal']) || 0)/10000)/100}
+                <div className={styles.bigFig + ' ' + styles.bold}>
+                  {Math.round((marginCall.getIn(['allocated', 'marginTotal']) || 0) / 10000) / 100}
                 </div>
                 <div className={styles.bold}>Million</div>
               </div>
@@ -182,37 +198,37 @@ export default class Selection extends React.Component {
                 <div className={styles.subSectionHeader}>Initial Margin</div>
                 <table className={styles.selTable + ( evlEmptyForIntMargin ? ' ' + styles.notAllocated : '')}>
                   <thead>
-                    <tr className={styles.bold}>
-                      <th></th>
-                      <th>Value(post <br/>haircut)</th>
-                      <th>CCY</th>
-                      <th>Haircut</th>
-                      <th>Value</th>
-                      <th>FX</th>
-                      <th>Venue</th>
-                      <th></th>
-                    </tr>
+                  <tr className={styles.bold}>
+                    <th></th>
+                    <th>Value(post <br/>haircut)</th>
+                    <th>CCY</th>
+                    <th>Haircut</th>
+                    <th>Value</th>
+                    <th>FX</th>
+                    <th>Venue</th>
+                    <th></th>
+                  </tr>
                   </thead>
                   <tbody>
                   { evlEmptyForIntMargin ?
-                      <tr>
-                        <td colSpan="8" className={styles.notAlcText}>Collateral has not been allocated</td>
-                      </tr> :
-                      this.checkIfExist(marginCall.getIn(['allocated', 'initialMargin'])).map(
-                        x => this.renderMargin(x,'initialMargin'))
+                    <tr>
+                      <td colSpan="8" className={styles.notAlcText}>Collateral has not been allocated</td>
+                    </tr> :
+                    this.checkIfExist(marginCall.getIn(['allocated', 'initialMargin'])).map(
+                      x => this.renderMargin(x, 'initialMargin'))
                   }
-                    <tr className={styles.bold}>
-                      <td>Sub-Total</td>
-                      <td>
-                        {numberWithCommas((marginCall.getIn(['allocated', 'initialMarginTotal']) || 0).toFixed(2))}
-                      </td>
-                      <td>USD</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
+                  <tr className={styles.bold}>
+                    <td>Sub-Total</td>
+                    <td>
+                      {numberWithCommas((marginCall.getIn(['allocated', 'initialMarginTotal']) || 0).toFixed(2))}
+                    </td>
+                    <td>USD</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
                   </tbody>
                 </table>
               </div>
@@ -221,25 +237,25 @@ export default class Selection extends React.Component {
                 <div className={styles.subSectionHeader}>Variation Margin</div>
                 <table className={styles.selTable + ( evlEmptyForVariMargin ? ' ' + styles.notAllocated : '')}>
                   <thead>
-                    <tr className={styles.bold}>
-                      <th></th>
-                      <th>Value(post <br/>haircut)</th>
-                      <th>CCY</th>
-                      <th>Haircut</th>
-                      <th>Value</th>
-                      <th>FX</th>
-                      <th>Venue</th>
-                      <th></th>
-                    </tr>
+                  <tr className={styles.bold}>
+                    <th></th>
+                    <th>Value(post <br/>haircut)</th>
+                    <th>CCY</th>
+                    <th>Haircut</th>
+                    <th>Value</th>
+                    <th>FX</th>
+                    <th>Venue</th>
+                    <th></th>
+                  </tr>
                   </thead>
                   <tbody>
 
                   { evlEmptyForVariMargin ?
-                        <tr>
-                          <td colSpan="8" className={styles.notAlcText}>Collateral has not been allocated</td>
-                        </tr> :
-                      this.checkIfExist(marginCall.getIn(['allocated', 'variationMargin'])).map(
-                        x => this.renderMargin(x,'variationMargin'))
+                    <tr>
+                      <td colSpan="8" className={styles.notAlcText}>Collateral has not been allocated</td>
+                    </tr> :
+                    this.checkIfExist(marginCall.getIn(['allocated', 'variationMargin'])).map(
+                      x => this.renderMargin(x, 'variationMargin'))
                   }
 
 
@@ -259,16 +275,17 @@ export default class Selection extends React.Component {
                 </table>
                 <table className={styles.selTable}>
                   <tbody>
-                    <tr className={styles.bold}>
-                      <td>Total</td>
-                      <td className={styles.totalTable1 + ( evlEmptyForMargin ? ' ' + styles.notAll : '' )}>{numberWithCommas((marginCall.getIn(['allocated', 'marginTotal']) || 0).toFixed(2))}</td>
-                      <td className={styles.totalTable2 + ( evlEmptyForMargin ? ' ' + styles.notAll : '' )}>USD</td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
+                  <tr className={styles.bold}>
+                    <td>Total</td>
+                    <td
+                      className={styles.totalTable1 + ( evlEmptyForMargin ? ' ' + styles.notAll : '' )}>{numberWithCommas((marginCall.getIn(['allocated', 'marginTotal']) || 0).toFixed(2))}</td>
+                    <td className={styles.totalTable2 + ( evlEmptyForMargin ? ' ' + styles.notAll : '' )}>USD</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
                   </tbody>
                 </table>
               </div>
