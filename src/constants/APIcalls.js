3055420e30edce4eda6946decb67bc69f6823daa
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-01-23 07:39:35 +0000

    OW-370 changes needed for the new qa.acuo.com instance

--- src/constants/APIcalls.js
+++ src/constants/APIcalls.js
@@ -1,10 +1,10 @@
 // Dashboard
 // export const DASHBOARD_URL = 'http://margin.acuo.com/acuo/api/margin/dashboard'
-export const DASHBOARD_URL = 'http://52.74.186.112:8081/dashboard'
+export const DASHBOARD_URL = 'http://qa.acuo.com:8181/dashboard'
 
 // Recon
 // export const RECON_URL = 'http://margin.acuo.com/acuo/api/margin/items/all/999'
-export const RECON_URL = 'http://52.74.186.112:8081/recon'
+export const RECON_URL = 'http://qa.acuo.com:8181/recon'
 
 // Pledge
 // export const OPTIMISATION_URL         = 'http://margin.acuo.com/acuo/api/pledge/settings/optimization/999'
@@ -12,13 +12,13 @@ export const RECON_URL = 'http://52.74.186.112:8081/recon'
 // export const COLLATERAL_URL           = 'http://52.74.186.112:8081/init-collateral'
 // export const ALLOCATE_COLLATERALS_URL = 'http://52.74.186.112:8081/allocate-selection'
 
-export const OPTIMISATION_URL         = 'http://52.74.186.112:8081/pledge/optimization'
-export const COLLATERAL_URL           = 'http://52.74.186.112:8081/pledge/init-collateral'
-export const MARGIN_SELECTION_URL     = 'http://52.74.186.112:8081/pledge/init-selection'
-export const ALLOCATE_COLLATERALS_URL = 'http://52.74.186.112:8081/pledge/allocate-selection'
+export const OPTIMISATION_URL         = 'http://qa.acuo.com:8181/pledge/optimization'
+export const COLLATERAL_URL           = 'http://qa.acuo.com:8181/pledge/init-collateral'
+export const MARGIN_SELECTION_URL     = 'http://qa.acuo.com:8181/pledge/init-selection'
+export const ALLOCATE_COLLATERALS_URL = 'http://qa.acuo.com:8181/pledge/allocate-selection'
 
 // POST
-export const UPLOAD_FILE_URL = 'http://valuation.acuo.com/acuo/api/upload'
-export const RECON_DATA_URL  = 'http://margin.acuo.com/acuo/api/margin/reconcile/'
+export const UPLOAD_FILE_URL = 'http://qa.acuo.com:9090/acuo/api/upload'
+export const RECON_DATA_URL  = 'http://qa.acuo.com:7070/acuo/api/margin/reconcile/'
 
 export const REMOVE_ASSET_ALLOCATION_URL = 'url to remove(and earmark) asset from margin call'
