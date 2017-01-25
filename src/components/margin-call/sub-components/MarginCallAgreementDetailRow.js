019386aed6034fd1351383d586c8b87b908a7424
Jun Jie Ng <jjng@palo-it.com>
2017-01-25 17:23:13 +0800

    OW-94 Update component to functional

--- src/components/margin-call/sub-components/MarginCallAgreementDetailRow.js
+++ src/components/margin-call/sub-components/MarginCallAgreementDetailRow.js
@@ -2,39 +2,34 @@ import React, {PropTypes} from 'react'
 import styles from './MarginCallAgreementDetailRow.css'
 
 
-export default class MarginCallAgreementRow extends React.Component {
-  render(){
-    const {
-      propAgreementDetailObj
-    } = this.props
+const MarginCallAgreementDetailRow = ({propAgreementDetailObj}) => (
+  <div>
+    <div className={styles.agreeRow + ' ' + styles.agreeHeaderRow}>
+      <div></div>
+      <div>Threshold</div>
+      <div>Min Transfer</div>
+      <div>Rounding</div>
+      <div>Net Required</div>
+      <div>Rate</div>
+      <div>Trade Count</div>
+      <div></div>
+    </div>
+    <div className={styles.agreeRow}>
+      <div></div>
+      <div>{propAgreementDetailObj.threshold}</div>
+      <div>{propAgreementDetailObj.minTransfer}</div>
+      <div>{propAgreementDetailObj.rounding}</div>
+      <div>{propAgreementDetailObj.netRequired}</div>
+      <div>{propAgreementDetailObj.rate}</div>
+      <div className={styles.boldText}>{propAgreementDetailObj.tradeCount}</div>
+      <div></div>
+    </div>
+  </div>
+)
 
-    return (
-      <div>
-        <div className={styles.agreeRow + ' ' + styles.agreeHeaderRow}>
-          <div></div>
-          <div>Threshold</div>
-          <div>Minimum Transfer</div>
-          <div>Rounding</div>
-          <div>Net Required</div>
-          <div>Rate</div>
-          <div>Trade Count</div>
-          <div></div>
-        </div>
-        <div className={styles.agreeRow}>
-          <div></div>
-          <div>{propAgreementDetailObj.threshold}</div>
-          <div>{propAgreementDetailObj.minTransfer}</div>
-          <div>{propAgreementDetailObj.rounding}</div>
-          <div>{propAgreementDetailObj.netRequired}</div>
-          <div>{propAgreementDetailObj.rate}</div>
-          <div className={styles.boldText}>{propAgreementDetailObj.tradeCount}</div>
-          <div></div>
-        </div>
-      </div>
-    )
-  }
-}
-
-MarginCallAgreementRow.PropTypes = {
+MarginCallAgreementDetailRow.PropTypes = {
   propAgreementDetailObj: PropTypes.object.isRequired
 }
+
+export default MarginCallAgreementDetailRow
+
