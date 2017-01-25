b07b9e24040f6c50ec0aa5e25a2849e9c93270fb
PALO IT-Andrian <paloit-andrian@Palo-It-Macboook.singnet.com.sg>
2017-01-25 17:21:24 +0800

    setting recon filter reducer function (WIP)

--- src/reducers/ReconReducer.js
+++ src/reducers/ReconReducer.js
@@ -1,12 +1,19 @@
 import * as ActionTypes from '../constants/ActionTypes'
 import {Map, List, fromJS} from 'immutable'
 
-const initState = Map({"items": List(), "filters": Map()})
+const initState = Map({"items":   List(),
+                       "filters": Map()})
 
-export default function reconReducer(state = initState, action, store = 'recon') {
+export default function reconReducer(state = initState, action) {
   switch(action.type) {
     case 'RECON_INIT_STATE':
       return state.set('items', fromJS(action.state))
+
+    case 'RECON_FILTER_SET':
+      const {filter, value} = action
+      return state.setIn(['filters', filter], value)
+
+    default:
+      return state
     }
-    return state
   }
