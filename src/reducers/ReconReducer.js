7564e11b8b4e62399baf76fe157d0de2aa6e9cff
PALO IT-Andrian <paloit-andrian@Palo-It-Macboook.singnet.com.sg>
2017-01-26 11:25:41 +0800

    updated reducer function for recon (‘RECON_FILTER_SET’)

--- src/reducers/ReconReducer.js
+++ src/reducers/ReconReducer.js
@@ -1,8 +1,15 @@
 import * as ActionTypes from '../constants/ActionTypes'
 import {Map, List, fromJS} from 'immutable'
+import _ from 'lodash'
 
 const initState = Map({"items":   List(),
-                       "filters": Map()})
+                       "filters": List()})
+
+const updateFilters = (filters, newFilters) => {
+  const newFilterNames = _.map(newFilters, 'name')
+  const untouchedFilters = _.filter(filters, ({name}) => (!newFilterNames.includes(name)))
+  return _.concat(untouchedFilters, newFilters)
+}
 
 export default function reconReducer(state = initState, action) {
   switch(action.type) {
@@ -10,8 +17,10 @@ export default function reconReducer(state = initState, action) {
       return state.set('items', fromJS(action.state))
 
     case 'RECON_FILTER_SET':
-      const {filter, value} = action
-      return state.setIn(['filters', filter], value)
+      const newFilters = action.value
+      const filters = state.get('filters').toJS()
+      const updatedFilters = updateFilters(filters, newFilters)
+      return state.set('filters', fromJS(updatedFilters))
 
     default:
       return state
