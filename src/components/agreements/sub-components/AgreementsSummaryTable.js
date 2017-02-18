2978ff5ecb195041a7a5708f0df695c45d97b252
Jun Jie Ng <jjng@palo-it.com>
2017-02-18 15:16:07 +0800

    OW-184 Add event handler to expand collapse icons

--- src/components/agreements/sub-components/AgreementsSummaryTable.js
+++ src/components/agreements/sub-components/AgreementsSummaryTable.js
@@ -1,10 +1,56 @@
 import React from 'react'
-import {plusBox} from '../../../../images/common'
+import {plusBox, minuxBox} from '../../../../images/common'
 import styles from './AgreementsSummaryTable.css'
 
 
 export default class AgreementsSummaryTable extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isCptySummaryExpanded: true
+    }
+
+    this.handleExpandCollapse = this.handleExpandCollapse.bind(this)
+  }
+
+  handleExpandCollapse(nextClickAction) {
+    this.setState({
+      isCptySummaryExpanded: nextClickAction
+    })
+  }
+
   render() {
+    let expandedRowOne =
+      <div className={styles.summaryTableRow}>
+        <div className={styles.summaryTableCell}>
+          <div className={styles.entityTypeContainer}>
+            Counterparty Leg
+          </div>
+        </div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+      </div>
+    let expandedRowTwo =
+      <div className={styles.summaryTableRow}>
+        <div className={styles.summaryTableCell}>
+          <div className={styles.entityTypeContainer}>
+            Counterparty Reg VM
+          </div>
+        </div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+        <div className={styles.summaryTableCell}>6</div>
+      </div>
+
+
     return (
       <div className={styles.summaryTable}>
         <div className={styles.summaryTableHeaderRow}>
@@ -28,7 +74,10 @@ export default class AgreementsSummaryTable extends React.Component {
         <div className={styles.summaryTableRow}>
           <div className={styles.summaryTableCell}>
             <div className={styles.entityTypeContainer}>
-              Counterparty <img src={plusBox} className={styles.plusMinusStyle}/>
+              Counterparty <img src={this.state.isCptySummaryExpanded ?
+              minuxBox : plusBox}
+                                className={styles.plusMinusStyle}
+                                onClick={() => this.handleExpandCollapse(!this.state.isCptySummaryExpanded)}/>
             </div>
           </div>
           <div className={styles.summaryTableCell}>12</div>
@@ -38,6 +87,16 @@ export default class AgreementsSummaryTable extends React.Component {
           <div className={styles.summaryTableCell}>12</div>
           <div className={styles.summaryTableCell}>12</div>
         </div>
+
+        {
+          this.state.isCptySummaryExpanded &&
+          expandedRowOne
+        }
+
+        {
+          this.state.isCptySummaryExpanded &&
+          expandedRowTwo
+        }
 
       </div>
     )
