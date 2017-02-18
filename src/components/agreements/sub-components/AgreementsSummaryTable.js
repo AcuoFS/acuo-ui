5a569b662312f7c651f2db20d4bb99b9147d10f6
Jun Jie Ng <jjng@palo-it.com>
2017-02-18 22:08:46 +0800

    OW-184 Refactoring

--- src/components/agreements/sub-components/AgreementsSummaryTable.js
+++ src/components/agreements/sub-components/AgreementsSummaryTable.js
@@ -1,97 +1,104 @@
 import React from 'react'
 import {plusBox, minuxBox} from '../../../../images/common'
-import * as TYPES from '../../../constants/AgreementsConstants'
+import * as CONSTANTS from '../../../constants/AgreementsConstants'
 import styles from './AgreementsSummaryTable.css'
 
 
-export default class AgreementsSummaryTable extends React.Component {
-  handleExpandCollapse(nextClickAction, propHandlerCptyExpand) {
-    propHandlerCptyExpand(nextClickAction)
-  }
+const createRowWithCommonProperties = (index, rowData, firstCellDom) => {
+  return <div className={styles.summaryTableRow} key={index}>
+    {firstCellDom}
+    <div className={styles.summaryTableCell}>{rowData.pendingNew}</div>
+    <div className={styles.summaryTableCell}>{rowData.pendingAssigned}</div>
+    <div className={styles.summaryTableCell}>{rowData.pendingRejected}</div>
+    <div className={styles.summaryTableCell}>{rowData.active}</div>
+    <div className={styles.summaryTableCell}>{rowData.changeRequest}</div>
+    <div className={styles.summaryTableCell}>{rowData.discontinuedReq}</div>
+  </div>
+}
 
-  render() {
-    const {
-      propSummaryData,
-      propIsCptySummaryExpanded,
-      propHandlerCptyExpand,
-      propTypeSelected
-    } = this.props
-
-    let clientDom, cptyDom, cptyBreakdown
-
-    if ((propTypeSelected == TYPES.AGREEMENT_TYPE_SELECTED_BOTH
-      || propTypeSelected == TYPES.AGREEMENT_TYPE_SELECTED_OUTGOING)
-      && propSummaryData.clientSummary) {
-      clientDom = <div className={styles.summaryTableRow}>
+/**
+ * Three different types of row: SUMMARY_ROW_CLIENT/SUMMARY_ROW_CPTY/SUMMARY_ROW_CPTY_DETAIL
+ *
+ * Most properties are largely the same, except for:
+ * 1) First cell on row
+ * 2) key property of row
+ *
+ * @param rowData
+ * @param rowType
+ * @param propIsCptySummaryExpanded
+ * @param propHandlerCptyExpand
+ * @param index
+ */
+const createSummaryTableRow = (rowData, rowType,
+                               propIsCptySummaryExpanded,
+                               propHandlerCptyExpand, index) => {
+  switch (rowType) {
+    case CONSTANTS.SUMMARY_ROW_CLIENT:
+      return createRowWithCommonProperties(CONSTANTS.SUMMARY_ROW_CLIENT, rowData,
         <div className={styles.summaryTableCell + ' ' + styles.clientDisplay}>
-          {propSummaryData.clientSummary.clientName}
+          {rowData.clientName}
         </div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.pendingNew}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.pendingAssigned}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.pendingRejected}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.active}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.changeRequest}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.clientSummary.discontinuedReq}</div>
-      </div>
-    }
-
-    if ((propTypeSelected == TYPES.AGREEMENT_TYPE_SELECTED_BOTH
-      || propTypeSelected == TYPES.AGREEMENT_TYPE_SELECTED_INCOMING)
-      && propSummaryData.cptySummary) {
-      cptyDom = <div className={styles.summaryTableRow}>
-        <div className={styles.summaryTableCell+ ' ' + styles.cptyDisplay}>
+      )
+    case CONSTANTS.SUMMARY_ROW_CPTY:
+      return createRowWithCommonProperties(CONSTANTS.SUMMARY_ROW_CPTY, rowData,
+        <div className={styles.summaryTableCell + ' ' + styles.cptyDisplay}>
           <div className={styles.entityTypeContainer}>
-            {propSummaryData.cptySummary.clientName} <img src={propIsCptySummaryExpanded ?
-            minuxBox : plusBox} className={styles.plusMinusStyle}
-                                                          onClick={() => this.handleExpandCollapse(
-                                                            !propIsCptySummaryExpanded,
-                                                            propHandlerCptyExpand)}/>
+            {rowData.clientName}
+            <img src={propIsCptySummaryExpanded ?
+              minuxBox : plusBox} className={styles.plusMinusStyle}
+                 onClick={() =>
+                   propHandlerCptyExpand(!propIsCptySummaryExpanded)}/>
           </div>
         </div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.pendingNew}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.pendingAssigned}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.pendingRejected}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.active}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.changeRequest}</div>
-        <div className={styles.summaryTableCell}>{propSummaryData.cptySummary.discontinuedReq}</div>
-      </div>
-
-      cptyBreakdown = propSummaryData.cptySummary.cptySummaryBreakdown.map((cptyRecord, index) => {
-        return <div className={styles.summaryTableRow} key={index}>
-          <div className={styles.summaryTableCell}>{cptyRecord.clientName}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.pendingNew}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.pendingAssigned}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.pendingRejected}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.active}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.changeRequest}</div>
-          <div className={styles.summaryTableCell}>{cptyRecord.discontinuedReq}</div>
-        </div>
-      })
-
-    }
-
-    return (
-      <div className={styles.summaryTable}>
-        <div className={styles.summaryTableHeaderRow}>
-          <div className={styles.summaryTableCell}></div>
-          <div className={styles.summaryTableCell}>Pending New</div>
-          <div className={styles.summaryTableCell}>Pending Assigned</div>
-          <div className={styles.summaryTableCell}>Rejected</div>
-          <div className={styles.summaryTableCell}>Active</div>
-          <div className={styles.summaryTableCell}>Change Request</div>
-          <div className={styles.summaryTableCell}>Discontinued Request</div>
-        </div>
-
-        {clientDom}
-
-        {cptyDom}
-
-        {
-          propIsCptySummaryExpanded &&
-          cptyBreakdown
-        }
-
-      </div>
-    )
+      )
+    default:
+      return createRowWithCommonProperties(index, rowData,
+        <div className={styles.summaryTableCell}>{rowData.clientName}</div>)
   }
 }
+
+const AgreementsSummaryTable = ({
+  propSummaryData,
+  propIsCptySummaryExpanded,
+  propHandlerCptyExpand,
+  propTypeSelected
+}) => {
+  return (
+    <div className={styles.summaryTable}>
+      <div className={styles.summaryTableHeaderRow}>
+        <div className={styles.summaryTableCell}></div>
+        <div className={styles.summaryTableCell}>Pending New</div>
+        <div className={styles.summaryTableCell}>Pending Assigned</div>
+        <div className={styles.summaryTableCell}>Rejected</div>
+        <div className={styles.summaryTableCell}>Active</div>
+        <div className={styles.summaryTableCell}>Change Request</div>
+        <div className={styles.summaryTableCell}>Discontinued Request</div>
+      </div>
+
+      {
+        (propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_BOTH
+        || propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_OUTGOING)
+        && propSummaryData.clientSummary &&
+        createSummaryTableRow(propSummaryData.clientSummary, CONSTANTS.SUMMARY_ROW_CLIENT, null, null, null)
+      }
+      {
+        (propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_BOTH
+        || propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_INCOMING)
+        && propSummaryData.cptySummary &&
+        createSummaryTableRow(propSummaryData.cptySummary, CONSTANTS.SUMMARY_ROW_CPTY, propIsCptySummaryExpanded,
+          propHandlerCptyExpand, null)
+      }
+      {
+        propIsCptySummaryExpanded &&
+        (propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_BOTH
+        || propTypeSelected == CONSTANTS.AGREEMENT_TYPE_SELECTED_INCOMING)
+        && propSummaryData.cptySummary &&
+        propSummaryData.cptySummary.cptySummaryBreakdown.map((cptyRecord, index) => (
+          createSummaryTableRow(cptyRecord, CONSTANTS.SUMMARY_ROW_CPTY_DETAIL, null, null, index)
+        ))
+      }
+    </div>
+  )
+}
+
+export default AgreementsSummaryTable
