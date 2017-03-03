8d194c652ab95a86b33ad80f51a88a5cc53620a3
Hicham Medkouri <hicham.medkouri@gmail.com>
2017-03-03 09:51:00 +0000

    release changes for QA

--- src/constants/APIcalls.js
+++ src/constants/APIcalls.js
@@ -4,19 +4,11 @@
 
 // Dashboard
 // export const DASHBOARD_URL = 'http://margin.acuo.com/acuo/api/margin/dashboard'
-<<<<<<< HEAD
-export const DASHBOARD_URL = 'http://qa.acuo.com:8181/dashboard'
-
-// Recon
-// export const RECON_URL = 'http://margin.acuo.com/acuo/api/margin/items/all/999'
-export const RECON_URL = 'http://qa.acuo.com:8181/recon'
-=======
 export const DASHBOARD_URL = CONFIG.DASHBOARD_URL
 
 // Recon
 // export const RECON_URL = 'http://margin.acuo.com/acuo/api/margin/items/all/999'
 export const RECON_URL = CONFIG.RECON_URL
->>>>>>> af51e18d15053da5afce9c1af7b3af00426b215d
 
 // Pledge
 // export const OPTIMISATION_URL         = 'http://margin.acuo.com/acuo/api/pledge/settings/optimization/999'
@@ -24,18 +16,6 @@ export const RECON_URL = CONFIG.RECON_URL
 // export const COLLATERAL_URL           = 'http://52.74.186.112:8081/init-collateral'
 // export const ALLOCATE_COLLATERALS_URL = 'http://52.74.186.112:8081/allocate-selection'
 
-<<<<<<< HEAD
-export const OPTIMISATION_URL         = 'http://qa.acuo.com:8181/pledge/optimization'
-export const COLLATERAL_URL           = 'http://qa.acuo.com:8181/pledge/init-new-collateral'
-export const MARGIN_SELECTION_URL     = 'http://qa.acuo.com:8181/pledge/init-selection'
-export const ALLOCATE_COLLATERALS_URL = 'http://qa.acuo.com:8181/pledge/allocate-selection'
-export const ALLOCATE_COLLATERALS_URL_NEW = 'http://qa.acuo.com:8181/pledge/allocate-selection-new'
-export const PLEDGE_ALLOCATIONS = 'http://qa.acuo.com:8181/pledge/pledge-allocation'
-
-// POST
-export const UPLOAD_FILE_URL = 'http://qa.acuo.com:9090/acuo/api/upload'
-export const RECON_DATA_URL  = 'http://qa.acuo.com:7070/acuo/api/margin/reconcile/'
-=======
 export const OPTIMISATION_URL         = CONFIG.OPTIMISATION_URL
 export const COLLATERAL_URL           = CONFIG.COLLATERAL_URL
 export const MARGIN_SELECTION_URL     = CONFIG.MARGIN_SELECTION_URL
@@ -46,13 +26,8 @@ export const PLEDGE_ALLOCATIONS = CONFIG.PLEDGE_ALLOCATIONS
 // POST
 export const UPLOAD_FILE_URL = CONFIG.UPLOAD_FILE_URL
 export const RECON_DATA_URL  = CONFIG.RECON_DATA_URL
->>>>>>> af51e18d15053da5afce9c1af7b3af00426b215d
 
 export const REMOVE_ASSET_ALLOCATION_URL = CONFIG.REMOVE_ASSET_ALLOCATION_URL
 
 // UNMATCHED PORTFOLIO
-<<<<<<< HEAD
-export const UNMATCHED_PORTFOLIO_URL = 'http://qa.acuo.com:8181/unmatched'
-=======
 export const UNMATCHED_PORTFOLIO_URL = CONFIG.UNMATCHED_PORTFOLIO_URL
->>>>>>> af51e18d15053da5afce9c1af7b3af00426b215d
