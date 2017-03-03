f10cfaafac6e3d7e45dc53d30b852584f52642b4
Jun Jie Ng <jjng@palo-it.com>
2017-03-03 16:08:00 +0800

    Update for webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,83 @@
 var path = require('path');
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  entry: {
-    javascript: './src/index.js',
-    html: './index.html'
-  },
+  entry: [
+    'whatwg-fetch',
+    './src/index.js',
+  ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    // filename: 'bundle.js',
+    filename: "[name].[hash].js"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
+        options: {
+          presets: [['es2015', {modules: false}], 'react']
         }
       },
       {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]",
-      },
-      {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]'
+            }
+          },
+          {loader: 'postcss-loader'}
+        ]
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              hash: 'sha512',
+              digest: 'hex',
+              name: '[hash].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              optimizationLevel: 7,
+              interlaced: false
+            }
+          }
         ]
       }
     ]
   },
   plugins: [
-    new webpack.ProvidePlugin({
-      'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    }),
     new CopyWebpackPlugin([
-      { from: './src/static/react-dropzone', to: './css/react-dropzone' },
-      { from: './src/static/reset.css', to: './css/reset.css'},
-      { from: './images', to: './images'}
+      {from: './src/static/react-dropzone', to: './css/react-dropzone'},
+      {from: './src/static/reset.css', to: './css/reset.css'},
+      {from: './images', to: './images'}
     ]),
     new webpack.DefinePlugin({
       CONFIG: JSON.stringify(require('./src/constants/config').get(process.env.DOCKER_ENV))
+    }),
+    // Plugin will automatically create and inject any js(output.[hash].bundle.js) into index.html
+    new HtmlWebpackPlugin({
+      template: 'index.template.ejs',
+      inject: 'body',
     })
   ],
   devServer: {
-    colors: true,
-    progress: true,
     historyApiFallback: true
   }
 };
