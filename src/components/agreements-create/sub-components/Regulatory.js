d65c4d6e67d1ece3bbc2c6dd7a1274bac4b03744
Jun Jie Ng <jjng@palo-it.com>
2017-03-27 17:49:44 +0800

    OW-225 Add textbooks after spit by call type

--- src/components/agreements-create/sub-components/Regulatory.js
+++ src/components/agreements-create/sub-components/Regulatory.js
@@ -13,7 +13,10 @@ export default class Regulatory extends React.Component {
     super(props)
 
     this.state = {
-      isSplit: false
+      isSplit: false,
+      iSplitByRoleVariation: false,
+      iSplitByRoleInitial: false,
+      iSplitByRoleNetted: false
     }
   }
 
@@ -37,7 +40,8 @@ export default class Regulatory extends React.Component {
       <div className={styles.rowGroup}>
         <div className={styles.line}>Regulatory CSA References</div>
         <div className={styles.line}>
-          <input type="text" className={styles.inputTextBox}/>
+          <input type="text" className={this.state.isSplit ? styles.inputTextBoxDisabled :
+            styles.inputTextBox} disabled={this.state.isSplit}/>
         </div>
       </div>
 
@@ -48,6 +52,60 @@ export default class Regulatory extends React.Component {
           &nbsp;Split by Call Type
         </div>
       </div>
+
+      {this.state.isSplit && <div className={styles.flexLine}>
+        <div className={styles.agreementsSectionLeft}>
+          <div className={styles.rowGroup}>
+            <div className={styles.line}>Variable Reference</div>
+            <div className={styles.line}>
+              <input type="text" className={this.state.iSplitByRoleVariation ? styles.inputTextBoxDisabled :
+                styles.inputTextBox} disabled={this.state.iSplitByRoleVariation}/>
+            </div>
+          </div>
+          <div className={styles.rowGroup}>
+            <div className={styles.line + ' ' + styles.flexLine}>
+              <ToggleSwitch propIsOn={this.state.iSplitByRoleVariation}
+                            propOnToggle={() => this.setState({iSplitByRoleVariation: !this.state.iSplitByRoleVariation})}/>
+              &nbsp;Split by Role
+            </div>
+          </div>
+
+        </div>
+        <div className={styles.agreementsSectionMiddle}>
+          <div className={styles.rowGroup}>
+            <div className={styles.line}>Initial Reference</div>
+            <div className={styles.line}>
+              <input type="text" className={this.state.iSplitByRoleInitial ? styles.inputTextBoxDisabled :
+                styles.inputTextBox} disabled={this.state.iSplitByRoleInitial}/>
+            </div>
+          </div>
+          <div className={styles.rowGroup}>
+            <div className={styles.line + ' ' + styles.flexLine}>
+              <ToggleSwitch propIsOn={this.state.iSplitByRoleInitial}
+                            propOnToggle={() => this.setState({iSplitByRoleInitial: !this.state.iSplitByRoleInitial})}/>
+              &nbsp;Split by Role
+            </div>
+          </div>
+        </div>
+        <div className={styles.agreementsSectionRight}>
+          <div className={styles.rowGroup}>
+            <div className={styles.line}>Netted Reference</div>
+            <div className={styles.line}>
+              <input type="text" className={this.state.iSplitByRoleNetted ? styles.inputTextBoxDisabled :
+                styles.inputTextBox} disabled={this.state.iSplitByRoleNetted}/>
+            </div>
+          </div>
+          <div className={styles.rowGroup}>
+            <div className={styles.line + ' ' + styles.flexLine}>
+              <ToggleSwitch propIsOn={this.state.iSplitByRoleNetted}
+                            propOnToggle={() => this.setState({iSplitByRoleNetted: !this.state.iSplitByRoleNetted})}/>
+              &nbsp;Split by Role
+            </div>
+          </div>
+
+        </div>
+
+      </div>}
 
       <ReferencesCallDriver propIsMenuRegulatory/>
       <ReferencesMarginTerms propIsMenuRegulatory/>
