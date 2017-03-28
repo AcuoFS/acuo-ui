a1b319e8dd64454348378fd9047840ba1ca9d093
Jun Jie Ng <jjng@palo-it.com>
2017-03-28 23:40:21 +0800

    OW-225 Add initial group and refactoring

--- src/components/agreements-create/sub-components/Regulatory.js
+++ src/components/agreements-create/sub-components/Regulatory.js
@@ -12,6 +12,17 @@ const BASIC_GROUP = 'BASIC'
 const VARIATION_GROUP = 'VARIATION_GROUP'
 const VARIATION_PLEDGOR_GROUP = 'VARIATION_PLEDGOR_GROUP'
 const VARIATION_SECURED_GROUP = 'VARIATION_SECURED_GROUP'
+const INITIAL_GROUP = 'INITIAL_GROUP'
+const INITIAL_PLEDGOR_GROUP = 'INITIAL_PLEDGOR_GROUP'
+const INITIAL_SECURED_GROUP = 'INITIAL_SECURED_GROUP'
+
+const VARIATION_DOM = 'variableInput'
+const VARIATION_PLEDGOR_DOM = 'variationPledgorInput'
+const VARIATION_SECURED_DOM = 'variationSecuredInput'
+const INITIAL_DOM = 'initialInput'
+const INITIAL_PLEDGOR_DOM = 'initialPledgorInput'
+const INITIAL_SECURED_DOM = 'initialSecuredInput'
+
 
 export default class Regulatory extends React.Component {
   constructor(props) {
@@ -24,6 +35,108 @@ export default class Regulatory extends React.Component {
       iSplitByRoleNetted: false,
       currentActiveGroup: BASIC_GROUP
     }
+  }
+
+  /**
+   * After updating, do the focus to the appropriate textbox
+   *
+   * @param prevProps
+   * @param prevState
+   */
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.propIsDisplay) {
+      switch (this.state.currentActiveGroup) {
+        case BASIC_GROUP:
+          this.regulatoryInput.focus()
+          break;
+        case VARIATION_GROUP:
+          this[VARIATION_DOM].focus()
+          break;
+        case VARIATION_PLEDGOR_GROUP:
+          this[VARIATION_PLEDGOR_DOM].focus()
+          break;
+        case VARIATION_SECURED_GROUP:
+          this[VARIATION_SECURED_DOM].focus()
+          break;
+        case INITIAL_GROUP:
+          this[INITIAL_DOM].focus()
+          break;
+        case INITIAL_PLEDGOR_GROUP:
+          this[INITIAL_PLEDGOR_DOM].focus()
+          break;
+        case INITIAL_SECURED_GROUP:
+          this[INITIAL_SECURED_DOM].focus()
+          break;
+        default:
+          this.regulatoryInput.focus()
+      }
+    }
+  }
+
+  getReferenceSectionGroup(groupName, label) {
+    return <div className={(this.state.currentActiveGroup !== groupName) && styles.hideForm}>
+      <ReferencesCallDriver propIsMenuRegulatory
+                            propPostfixLabel={label}/>
+      <ReferencesMarginTerms propIsMenuRegulatory
+                             propPostfixLabel={label}/>
+      <ReferencesCallIssuance propIsMenuRegulatory
+                              propPostfixLabel={label}/>
+    </div>
+
+  }
+
+  getAfterSplitReference(contClass, splitByRoleState,
+                         baseGroup, baseDisplay, baseDom,
+                         pledgorGroup, pledgorDisplay, pledgorDom,
+                         securedGroup, securedDisplay, securedDom) {
+
+    return <div className={contClass}>
+      <div className={styles.rowGroup}>
+        <div className={styles.line}>{baseDisplay}</div>
+        <div className={styles.line}>
+          <input type="text" className={this.state[splitByRoleState] ?
+            styles.inputTextBoxDisabled : styles.inputTextBox}
+                 disabled={this.state[splitByRoleState]}
+                 ref={(dom) => this[baseDom] = dom}
+                 onFocus={() => this.setState({currentActiveGroup: baseGroup})}/>
+
+        </div>
+      </div>
+      <div className={styles.rowGroup}>
+        <div className={styles.line + ' ' + styles.flexLine}>
+          <ToggleSwitch propIsOn={this.state[splitByRoleState]}
+                        propOnToggle={() => this.setState({
+                          [splitByRoleState]: !this.state[splitByRoleState],
+                          currentActiveGroup: this.state[splitByRoleState] ?
+                            baseGroup : pledgorGroup
+                        })}/>
+          &nbsp;Split by Role
+        </div>
+      </div>
+
+      {this.state[splitByRoleState] && <div className={styles.flexColumn}>
+        <div className={styles.rowGroup}>
+          <div className={styles.line}>{pledgorDisplay}</div>
+          <div className={styles.line}>
+            <input type="text" className={styles.inputTextBox}
+                   ref={(dom) => this[pledgorDom] = dom}
+                   onFocus={() => this.setState({currentActiveGroup: pledgorGroup})}/>
+
+          </div>
+        </div>
+        <div className={styles.rowGroup}>
+          <div className={styles.line}>{securedDisplay}</div>
+          <div className={styles.line}>
+            <input type="text" className={styles.inputTextBox}
+                   ref={(dom) => this[securedDom] = dom}
+                   onFocus={() => this.setState({currentActiveGroup: securedGroup})}/>
+
+          </div>
+        </div>
+      </div>}
+
+    </div>
+
   }
 
   render() {
@@ -55,100 +168,26 @@ export default class Regulatory extends React.Component {
         <div className={styles.line + ' ' + styles.flexLine}>
           <ToggleSwitch propIsOn={this.state.isSplit}
                         propOnToggle={() =>
-                          // pass a callback as second param to make sure setState is fired
                           this.setState({
-                              isSplit: !this.state.isSplit,
-                              currentActiveGroup: this.state.isSplit ? BASIC_GROUP : VARIATION_GROUP
-                            },
-                            () => this.state.isSplit ?
-                              this.variableInput.focus() : this.regulatoryInput.focus())
-                        }/>
+                            isSplit: !this.state.isSplit,
+                            currentActiveGroup: this.state.isSplit ? BASIC_GROUP :
+                              (this.state.iSplitByRoleVariation ? VARIATION_PLEDGOR_GROUP : VARIATION_GROUP)
+                          })}/>
           &nbsp;Split by Call Type
         </div>
       </div>
 
       {this.state.isSplit && <div className={styles.flexCont}>
-        <div className={styles.agreementsSectionLeft}>
-          <div className={styles.rowGroup}>
-            <div className={styles.line}>Variable Reference</div>
-            <div className={styles.line}>
-              <input type="text" className={this.state.iSplitByRoleVariation ?
-                styles.inputTextBoxDisabled : styles.inputTextBox}
-                     disabled={this.state.iSplitByRoleVariation}
-                     ref={(dom) => this.variableInput = dom}
-                     onFocus={() => this.setState({currentActiveGroup: VARIATION_GROUP})}/>
 
-            </div>
-          </div>
-          <div className={styles.rowGroup}>
-            <div className={styles.line + ' ' + styles.flexLine}>
-              <ToggleSwitch propIsOn={this.state.iSplitByRoleVariation}
-                            propOnToggle={() => this.setState({
-                                iSplitByRoleVariation: !this.state.iSplitByRoleVariation,
-                                currentActiveGroup: this.state.iSplitByRoleVariation ?
-                                  VARIATION_GROUP : VARIATION_PLEDGOR_GROUP
-                              },
-                              () => this.state.iSplitByRoleVariation ?
-                                this.variationPledgorInput.focus() : this.variableInput.focus())}/>
-              &nbsp;Split by Role
-            </div>
-          </div>
+        {this.getAfterSplitReference(styles.agreementsSectionLeft, 'iSplitByRoleVariation',
+          VARIATION_GROUP, 'Variable Reference', VARIATION_DOM,
+          VARIATION_PLEDGOR_GROUP, 'Variation Pledgor Reference', VARIATION_PLEDGOR_DOM,
+          VARIATION_SECURED_GROUP, 'Variation Secured Reference', VARIATION_SECURED_DOM)}
 
-          {this.state.iSplitByRoleVariation && <div className={styles.flexColumn}>
-            <div className={styles.rowGroup}>
-              <div className={styles.line}>Variation Pledgor Reference</div>
-              <div className={styles.line}>
-                <input type="text" className={styles.inputTextBox}
-                       ref={(dom) => this.variationPledgorInput = dom}
-                       onFocus={() => this.setState({currentActiveGroup: VARIATION_PLEDGOR_GROUP})}/>
-
-              </div>
-            </div>
-            <div className={styles.rowGroup}>
-              <div className={styles.line}>Variation Secured Reference</div>
-              <div className={styles.line}>
-                <input type="text" className={styles.inputTextBox}
-                       onFocus={() => this.setState({currentActiveGroup: VARIATION_SECURED_GROUP})}/>
-
-              </div>
-            </div>
-          </div>}
-
-        </div>
-        <div className={styles.agreementsSectionMiddle}>
-          <div className={styles.rowGroup}>
-            <div className={styles.line}>Initial Reference</div>
-            <div className={styles.line}>
-              <input type="text" className={this.state.iSplitByRoleInitial ? styles.inputTextBoxDisabled :
-                styles.inputTextBox} disabled={this.state.iSplitByRoleInitial}/>
-            </div>
-          </div>
-          <div className={styles.rowGroup}>
-            <div className={styles.line + ' ' + styles.flexLine}>
-              <ToggleSwitch propIsOn={this.state.iSplitByRoleInitial}
-                            propOnToggle={() => this.setState({
-                              iSplitByRoleInitial: !this.state.iSplitByRoleInitial
-                            })}/>
-              &nbsp;Split by Role
-            </div>
-          </div>
-
-          {this.state.iSplitByRoleInitial && <div className={styles.flexColumn}>
-            <div className={styles.rowGroup}>
-              <div className={styles.line}>Initial Pledgor Reference</div>
-              <div className={styles.line}>
-                <input type="text" className={styles.inputTextBox}/>
-              </div>
-            </div>
-            <div className={styles.rowGroup}>
-              <div className={styles.line}>Initial Secured Reference</div>
-              <div className={styles.line}>
-                <input type="text" className={styles.inputTextBox}/>
-              </div>
-            </div>
-          </div>}
-
-        </div>
+        {this.getAfterSplitReference(styles.agreementsSectionMiddle, 'iSplitByRoleInitial',
+          INITIAL_GROUP, 'Initial Reference', INITIAL_DOM,
+          INITIAL_PLEDGOR_GROUP, 'Initial Pledgor Reference', INITIAL_PLEDGOR_DOM,
+          INITIAL_SECURED_GROUP, 'Initial Secured Reference', INITIAL_SECURED_DOM)}
 
         <div className={styles.agreementsSectionRight}>
           <div className={styles.rowGroup}>
@@ -187,41 +226,15 @@ export default class Regulatory extends React.Component {
 
       </div>}
 
-      <div className={(this.state.currentActiveGroup !== BASIC_GROUP) && styles.hideForm}>
-        <ReferencesCallDriver propIsMenuRegulatory
-                              propPostfixLabel={' - Regulatory CSA'}/>
-        <ReferencesMarginTerms propIsMenuRegulatory
-                               propPostfixLabel={' - Regulatory CSA'}/>
-        <ReferencesCallIssuance propIsMenuRegulatory
-                                propPostfixLabel={' - Regulatory CSA'}/>
-      </div>
+      {this.getReferenceSectionGroup(BASIC_GROUP, ' - Regulatory CSA')}
 
-      <div className={(this.state.currentActiveGroup !== VARIATION_GROUP) && styles.hideForm}>
-        <ReferencesCallDriver propIsMenuRegulatory
-                              propPostfixLabel={' - Regulatory CSA Variation'}/>
-        <ReferencesMarginTerms propIsMenuRegulatory
-                               propPostfixLabel={' - Regulatory CSA Variation'}/>
-        <ReferencesCallIssuance propIsMenuRegulatory
-                                propPostfixLabel={' - Regulatory CSA Variation'}/>
-      </div>
+      {this.getReferenceSectionGroup(VARIATION_GROUP, ' - Regulatory CSA Variation')}
+      {this.getReferenceSectionGroup(VARIATION_PLEDGOR_GROUP, ' - Regulatory CSA Variation Pledgor')}
+      {this.getReferenceSectionGroup(VARIATION_SECURED_GROUP, ' - Regulatory CSA Variation Secured')}
 
-      <div className={(this.state.currentActiveGroup !== VARIATION_PLEDGOR_GROUP) && styles.hideForm}>
-        <ReferencesCallDriver propIsMenuRegulatory
-                              propPostfixLabel={' - Regulatory CSA Variation Pledgor'}/>
-        <ReferencesMarginTerms propIsMenuRegulatory
-                               propPostfixLabel={' - Regulatory CSA Variation Pledgor'}/>
-        <ReferencesCallIssuance propIsMenuRegulatory
-                                propPostfixLabel={' - Regulatory CSA Variation Pledgor'}/>
-      </div>
-
-      <div className={(this.state.currentActiveGroup !== VARIATION_SECURED_GROUP) && styles.hideForm}>
-        <ReferencesCallDriver propIsMenuRegulatory
-                              propPostfixLabel={' - Regulatory CSA Variation Secured'}/>
-        <ReferencesMarginTerms propIsMenuRegulatory
-                               propPostfixLabel={' - Regulatory CSA Variation Secured'}/>
-        <ReferencesCallIssuance propIsMenuRegulatory
-                                propPostfixLabel={' - Regulatory CSA Variation Secured'}/>
-      </div>
+      {this.getReferenceSectionGroup(INITIAL_GROUP, ' - Regulatory CSA Initial')}
+      {this.getReferenceSectionGroup(INITIAL_PLEDGOR_GROUP, ' - Regulatory CSA Initial Pledgor')}
+      {this.getReferenceSectionGroup(INITIAL_SECURED_GROUP, ' - Regulatory CSA Initial Secured')}
 
     </div>
   }
