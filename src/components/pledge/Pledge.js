6a61dca324059b6de88cb6ea5cdf6e4cb1e48ed4
Jun Jie Ng <jjng@palo-it.com>
2017-04-05 15:16:08 +0800

    Revert back changes on Pledge.js

--- src/components/pledge/Pledge.js
+++ src/components/pledge/Pledge.js
@@ -2,10 +2,12 @@
  * Created by panyong on 22/11/16.
  */
 import React from 'react'
+import {render} from 'react-dom'
+import OptItem from './sub-components/OptItem'
+import ChooseCalls from './sub-components/ChooseCalls'
 import Selection from '../pledge-selection/Selection'
 import CollateralWidgetContainer from '../../containers/CollateralWidgetContainer'
 import {OPTIMISATION_URL,MARGIN_SELECTION_URL} from '../../constants/APIcalls'
-import OptimisationWidget from '../pledge-optimisation/OptimisationWidget'
 import styles from './Pledge.css'
 
 import { List } from 'immutable'
@@ -17,11 +19,11 @@ class Pledge extends React.Component {
       open: true,
       dropdown: "./images/common/minusbox.png",
 
-      toggleColwidthL: styles.minDivL, 
+      toggleColwidthL: styles.minDivL,
       toggleColwidthR: styles.expandDivR,
 
       toggleHideCol: styles.showCol,
-      toggleShowHideL : false, 
+      toggleShowHideL : false,
       toggleShowHideR : true,
       sideways: "./images/pledge/sideways-min.png",
       selectionSideway: "./images/pledge/sideways-max.png",
@@ -31,6 +33,7 @@ class Pledge extends React.Component {
     }
     this.changeSideways = this.changeSideways.bind(this)
     this.onPledgeButtonClick = this.onPledgeButtonClick.bind(this)
+    // this.renderCollateralItems = this.renderCollateralItems.bind(this)
 
     fetch(OPTIMISATION_URL).then(response => {
       return response.json()
@@ -71,6 +74,17 @@ class Pledge extends React.Component {
     }
   }
 
+  renderOptItems(optimisation, onUpdateOptimisationSettings){
+    if(optimisation) {
+      return optimisation.map((x,index) => {
+        return (<OptItem sldName={x.get('name')}
+                         allocation={x.get('rating')}
+                         onUpdate={onUpdateOptimisationSettings}
+                         key={index}/>)
+      })
+    }
+  }
+
   renderSelection(x, onTogglePendingAllocation, pendingAllocation, index){
     return (<Selection  sideways={this.state.selectionSideway}
                         clicked={this.changeSideways}
@@ -102,44 +116,87 @@ class Pledge extends React.Component {
         : 0 )
   }
 
+  isShowPledgeBtn(selection) {
+    if (selection) {
+      for (const statement of selection) {
+        if (statement.get('allocated')) {
+          return true
+        }
+      }
+    }
+    return false
+  }
+
   render() {
-    const {
-      selection, onTogglePendingAllocation,
-      pendingAllocation
-    } = this.props
+    const { optimisation, selection, onUpdateOptimisationSettings, onTogglePendingAllocation,
+      pendingAllocation, sliderCheckbox, onToggleCheckall, onAllocate,
+      onPledge} = this.props
 
     return (
 
-        <div className={styles.pledgeContainer}>
-          <div className={styles.sliderAndStatus}>
-
-            <OptimisationWidget {...this.props}/>
-
-            <div className={styles.panel} id={styles.pleStatus}>
-              {/*<div className={styles.panelTitle}>Pledge Status</div>*/}
-              <img src="./images/pledge/Pledge-status-widget.png"/>
+      <div className={styles.pledgeContainer}>
+        <div className={styles.sliderAndStatus}>
+          <div className={styles.panel} id={styles.optSetting}>
+            <div className={styles.panelTitle}>Optimization Setting <img src={'./images/pledge/locked.png'} /></div>
+            <div className={styles.optPnlWrap}>
+              {this.renderOptItems(optimisation, onUpdateOptimisationSettings)}
             </div>
+            <div className={styles.buttonHolder}>
+              <ChooseCalls tickImg={sliderCheckbox[0]} tickState={sliderCheckbox[1]}
+                           tickClick={onToggleCheckall}/>
+
+              <div className={styles.optButton + ' ' +
+              (this.checkIfExist(pendingAllocation).size > 0 ? '' : styles.btnDisabled )}
+                   onClick={() => onAllocate(pendingAllocation.toJS(), optimisation.toJS())}>
+                Allocate
+              </div>
+
+              {/*<div className={styles.optButton + ' ' +*/}
+              {/*(this.checkIfExist(selection).reduce(this.sumOfIMVM, 0) > 0*/}
+              {/*? styles.optBtnPledge*/}
+              {/*: styles.btnDisabled )}*/}
+              {/*onClick={() => onPledge(selection.toJS())}>*/}
+              {/*Pledge*/}
+              {/*</div>*/}
+
+              <div className={styles.optButton + ' ' +
+              (this.isShowPledgeBtn(selection)
+                ? styles.optBtnPledge
+                : styles.btnDisabled )}
+                   onClick={() => onPledge(selection.toJS())}>
+                Pledge
+              </div>
+
+
+            </div>
+            {/* change btnEnabled to btnDisabled to disable the button*/}
           </div>
 
-          <div className={styles.secDivider}/>
-
-          <div className={styles.flexContainer}>
-
-            <div className={styles.col_L + ' ' + this.state.toggleColwidthL}>
-
-              {this.checkIfExist(selection)
-                .map((x, index) => this.renderSelection(x, onTogglePendingAllocation, pendingAllocation, index))}
-
-            </div>
-
-            <CollateralWidgetContainer
-              toggleColwidthR={this.state.toggleColwidthR}
-              sideways={this.state.sideways}
-              open={this.state.open}
-              changeSideways={this.changeSideways}/>
-
+          <div className={styles.panel} id={styles.pleStatus}>
+            {/*<div className={styles.panelTitle}>Pledge Status</div>*/}
+            <img src="./images/pledge/Pledge-status-widget.png"/>
           </div>
         </div>
+
+        <div className={styles.secDivider}></div>
+
+        <div className={styles.flexContainer}>
+
+          <div className={styles.col_L + ' ' + this.state.toggleColwidthL}>
+
+            {this.checkIfExist(selection)
+              .map((x, index) => this.renderSelection(x, onTogglePendingAllocation, pendingAllocation, index))}
+
+          </div>
+
+          <CollateralWidgetContainer
+            toggleColwidthR={this.state.toggleColwidthR}
+            sideways={this.state.sideways}
+            open={this.state.open}
+            changeSideways={this.changeSideways}/>
+
+        </div>
+      </div>
 
     )
   }
