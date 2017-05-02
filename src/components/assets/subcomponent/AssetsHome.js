d5f43cf85a16757690025cb0acf7fc8c1be2e1bd
Jonathan Tan <jonalogy@gmail.com>
2017-05-02 14:52:04 +0800

    Changed AssetsHome component from a class based to a functional component

--- src/components/assets/subcomponent/AssetsHome.js
+++ src/components/assets/subcomponent/AssetsHome.js
@@ -1,27 +1,35 @@
 import React from 'react';
+import { connect } from "react-redux"
 import styles from './AssetsPanel.css'
 import PanelWindow from './deployedViews/PanelWindow.js'
 import AssetsHomeTableView from './deployedViews/TableView/AssetsHomeTableView.js'
+/*Actions*/
+import {AssetsPanel} from '../../../actions/AssetsActions.js'
+//Mock Data
+import { VarMarginTableStyle, InitMarginTableStyle } from "../mockData/mockData.js"
 
-export default class AssetsHomeComponent extends React.Component {
-   componentDidMount(){
-   }
 
-   render(){
-     return(
-       <div className={ styles.assetsPanelFrame } >
-         <div  className={ styles.assetsPanelHeader} >
-           <span  className={ styles.assetsPanelTitleText }> At Home </span>
-           <img className={styles.assetsPanelHeaderSideExpandBtn}
-                src="images/assets_deployed/minimize-sideways.svg"
-                onClick={ ()=>{ console.log(this.state) }}/>
-         </div>
-         
-         <PanelWindow>
-           <AssetsHomeTableView />
-         </PanelWindow>
+const AssetsHome = ()=>{
 
+   return(
+     <div className={ styles.assetsPanelFrame } >
+       <div  className={ styles.assetsPanelHeader} >
+         <span  className={ styles.assetsPanelTitleText }> At Home </span>
+         <img className={styles.assetsPanelHeaderSideExpandBtn}
+              src="images/assets_deployed/minimize-sideways.svg"
+              onClick={ ()=>{}}/>
        </div>
-     )
-   }
+
+       <PanelWindow>
+         <AssetsHomeTableView />
+       </PanelWindow>
+
+     </div>
+   )
 }
+/*----------------------------------------------------------------------------*/
+const AssetsHomeComponent = connect(
+ mapDispatchToProps
+)(AssetsHome)
+
+export default AssetsHomeComponent
