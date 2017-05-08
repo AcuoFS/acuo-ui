bb06750ee4db0c46ffcab5cc7533ce800a83153f
Jonathan Tan <jontan_paloit@Tans-MacBook-Pro.local>
2017-05-08 16:07:24 +0800

    Added static table to AssetsHomePanel; Changed font-weight for table headers back to normal

--- src/components/assets/subcomponent/deployedViews/TableView/AssetsHomeTableView.js
+++ src/components/assets/subcomponent/deployedViews/TableView/AssetsHomeTableView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './AssetsHomeTableView.css'
 import NavBar from '../NavBar.js'
+import NavBarStyle from './../NavBar.css'
 import Table from '../tableUI/tableUI.js'
 
 
@@ -14,7 +15,8 @@ const AssetsHomeTableView = (props)=>{
     <div className={styles.tableView}>
       <Table.RowGroup style={ TableStyle.RowGroupStyle } >
         <NavBar>
-          Blah
+          <div className={NavBarStyle.tabs}>Principal</div>
+          <div className={NavBarStyle.tabs + " " + NavBarStyle.selected}>Pledge</div>
         </NavBar>
       </Table.RowGroup>
 
@@ -22,7 +24,7 @@ const AssetsHomeTableView = (props)=>{
         <Table.DataRow content={Content.Header} style={TableStyle.HeaderRow} />
 
         {
-         Content.RowData.map((row,idx)=>( <Table.DataRow content={row} style={TableStyle.DataRow} key={idx} /> ) )
+           Content.RowData.map((row,idx)=>( <Table.DataRow content={row} style={TableStyle.DataRow} key={idx} /> ) )
         }
 
       </Table.ColGroup>
