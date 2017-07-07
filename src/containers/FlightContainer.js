12188bc8f0692711c07a0502bb5e8a73f0299fd5
Rui <rchai@palo-it.com>
2017-07-07 14:00:01 +0800

    OW-95 - creation of unique dates

--- src/containers/FlightContainer.js
+++ src/containers/FlightContainer.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {FlightComponent} from '../components'
+import { connect } from 'react-redux'
+import _ from 'lodash'
+
+import { FlightComponent } from '../components'
 import flightGroupArrival from '../components/assets-flight/mockFlightsArrival'
-import {search} from "../actions/DeployedActions.js"
+import { search } from "../actions/DeployedActions.js"
 
 const mapStateToProps = state => {
- return{ departures: state.DeployedReducer.get('departures').toJS(),
+ return { departures: state.DeployedReducer.get('departures').toJS(),
          arrivals: flightGroupArrival,
          arrivals_searchText: state.DeployedReducer.get('arrivals_searchText'),
-         departures_searchText: state.DeployedReducer.get('departures_searchText')
+         departures_searchText: state.DeployedReducer.get('departures_searchText'),
+         departureDates: getUniqueDates(state.DeployedReducer.get('departures').toJS())
         }
 }
 
@@ -18,6 +21,10 @@ const mapDispatchToProps = dispatch => {
     departureSearch: (searchText)=>{ dispatch(search.departures(searchText))}
   }
 }
+
+const getUniqueDates = departuresList =>
+  _.reduce(departuresList, (sum, x) => _.union(sum, [(new Date(x.header.time).getDate()) + ' ' + monthNames[(new Date(x.header.time).getMonth())]]), [])
+
 
 const FlightContainer = connect(
   mapStateToProps,
--- src/reducers/DeployedReducer.js
+++ src/reducers/DeployedReducer.js
@@ -2,7 +2,9 @@ import {
   INIT_DEPARTURES
 } from '../constants/ActionTypes'
 import {List, Map, fromJS} from 'immutable'
+import _ from 'lodash'
 
+import { clearTime } from './../utils'
 
 const INITIAL_STATE = Map({
   departures: List(),
@@ -14,7 +16,17 @@ const INITIAL_STATE = Map({
 const DeployedReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case INIT_DEPARTURES:
-      return state.set('departures', fromJS(action.departures))
+      return state.withMutations((state) =>
+        state
+          .set('departures', fromJS(action.departures))
+          .set('departureDatesList', fromJS(
+              _.reduce(action.departures, (sum, x) => {
+                const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+                return _.union(sum, [
+                  (new Date(x.header.time).getDate()) + ' ' + monthNames[(new Date(x.header.time).getMonth())]
+                ])
+              }, []))))
     case "DEPARTURES_SEARCHTEXT":
       return state.set('departures_searchText', fromJS(action.payload))
     case "ARRIVALS_SEARCHTEXT":
