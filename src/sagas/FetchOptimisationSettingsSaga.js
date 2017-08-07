a69770c7346b5f058bc1805f0e365c3f817f489b
Rui <rchai@palo-it.com>
2017-08-07 10:49:20 +0800

    OW-643 - migrated init optimisation settings to sagas

---
+++ src/sagas/FetchOptimisationSettingsSaga.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Rui on 4/8/17.
+ */
+
+import { OPTIMISATION_URL } from './../constants/APIcalls'
+
+export const FetchOptimisationSettingsSaga = () =>{
+  return(
+    fetch(OPTIMISATION_URL)
+      .then(response => response.json())
+      .then(json => {
+        return json
+      })
+  )
+}
