0aa2f1c36c6fbe8d7b38298368f17cc2a100fc61
Rui <rchai@palo-it.com>
2017-08-23 14:52:55 +0800

    OW-671 - login error messages

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import styles from './Login.css'
-import { hashHistory } from 'react-router'
+import {hashHistory} from 'react-router'
 
-
-export default class LoginComponent extends React.Component{
-  constructor(props){
-   super(props)
-   this.state = { showPassword : false,
-                 passwordError : "",
-                      password : "@Password1",
-                      username : 'user@acuocpty.com',
-                 inputPassword : "@Password1" }
+export default class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showPassword: false,
+      username: 'user@acuocpty.com',
+      password: "@Password1"
+    }
   }
 
-  setPassword(value){
-    this.setState({password: e.currentTarget.value})
-  }
-
-  render(){
-     return(
+  render() {
+    const { processingLogin, wrongCredentials, onLogin } = this.props
+    return (
       <div className={styles.container}>
         <div className={styles.box}>
           <img className={styles.logo} src={'./images/login/login_logo.png'} alt=""/>
@@ -29,42 +25,48 @@ export default class LoginComponent extends React.Component{
             <div className={styles.input}>
               <input type="text"
                      value={this.state.username}
-                     onChange={ (e)=>{this.setState({username: e.currentTarget.value})} }  />
+                     onChange={ (e) => {
+                       this.setState({username: e.currentTarget.value})
+                     } }/>
             </div>
           </div>
 
           <div className={styles.row}>
             <div className={styles.label}>
               password
-              <span onClick={ ()=>{ this.setState( {showPassword: !this.state.showPassword} ) } } >
-               {(this.state.showPassword? "hide" : "show")}
+              <span onClick={() => {
+                this.setState({showPassword: !this.state.showPassword})
+              }}>
+               {(this.state.showPassword ? "hide" : "show")}
               </span>
             </div>
-            <div className={ styles.pw_error }>
-              {this.state.passwordError}
-            </div>
             <div className={styles.input}>
-              <input type={(this.state.showPassword? "text" : "password")}
-                     value={this.state.inputPassword}
-                     onChange={ (e)=>{this.setState({inputPassword: e.currentTarget.value})} }
-                />
+              <input type={(this.state.showPassword ? "text" : "password")}
+                     value={this.state.password}
+                     onChange={ (e) => {
+                       this.setState({password: e.currentTarget.value})
+                     } }
+              />
             </div>
+            {wrongCredentials &&
+              <div className={ styles.pw_error }>
+                Login failed, please check your credentials
+              </div>}
           </div>
 
           <div className={styles.buttonHolder}>
-            <button className={(this.props.processingLogin ? styles.disabled : '')}
+            <button className={(processingLogin ? styles.disabled : '')}
                     onClick={ () => {
                       this.setState({passwordError: ""})
-                      this.props.onLogin(this.state.username, this.state.inputPassword)}}>
+                      onLogin(this.state.username, this.state.password)
+                    }}>
               sign in
             </button>
           </div>
         </div>
       </div>
-   )
+    )
   }
 }
-
-
 
 // export default LoginComponent
--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -5,11 +5,12 @@ import { doLogin } from './../actions/LoginActions'
 const _default = false
 
 const mapStateToProps = (state) => ({
-  processingLogin: state.CommonReducer.get('processingLogin') || _default
+  processingLogin: state.CommonReducer.get('processingLogin') || _default,
+  wrongCredentials: state.CommonReducer.get('wrongCredentials')
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  onLogin: (user, pass) => dispatch(doLogin(user, pass))
+  onLogin: (user, pass) => dispatch(doLogin(user, pass)),
 })
 
 const LoginContainer = connect(mapStateToProps, mapDispatchToProps)(LoginComponent)
