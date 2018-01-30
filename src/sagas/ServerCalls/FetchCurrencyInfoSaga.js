917e736b26208d0339b80cebe5f5bcbe3697623b
Rui <rchai@palo-it.com>
2018-01-30 11:39:34 +0800

    accident

--- src/sagas/ServerCalls/FetchCurrencyInfoSaga.js
+++ src/sagas/ServerCalls/FetchCurrencyInfoSaga.js
@@ -1,12 +1,11 @@
 /**
  * Created by Rui on 15/11/17.
  */
-import axios from 'axios'
 
 import { FETCH_CURRENCY_INFO } from '../../constants/APIcalls'
 
 export const FetchCurrencyInfoSaga = () => (
-  axios.get(`${FETCH_CURRENCY_INFO}/${window.localStorage.clientId}`).then((response) => {
+  fetch(`${FETCH_CURRENCY_INFO}/${window.localStorage.clientId}`).then((response) => {
     return response.json()
   }).then((obj) => {
     return obj
